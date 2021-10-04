ad5a0beda688e290ad17e85174dc40ba4a4fb6ba
XuanRanDev <3135419729@qq.com>
2021-10-04 12:17:51 +0800

    Test Change commit author2

--- app/src/main/java/com/xuanran/githubtest/MainActivity.java
+++ app/src/main/java/com/xuanran/githubtest/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"This is a test log.");
+        Log.d(TAG,"This is a test log.");
     }
 }
