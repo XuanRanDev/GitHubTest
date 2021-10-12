0ef4b903ea81829f601fdb764ee3af258a4a3121
XuanRanDev <3135419729@qq.com>
2021-10-12 21:45:57 +0800

    commit test 1

--- app/src/main/java/com/xuanran/githubtest/MainActivity.java
+++ app/src/main/java/com/xuanran/githubtest/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG,"This is a test log.");
         Log.d(TAG,"This is a test log.");
         Log.d(TAG,"This is a test Pull requests");
+        Log.d(TAG,"This is a test Pull requests");
     }
 }
